a74a66bf56897bad5f56768df65ca0625c85e6b4
ir2sora <ir2sora@gmail.com>
2015-04-12 21:51:33 +0300

    simplify running redmine

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Redmine 2.3 - Ruby 1.8
 
 Run:
-1. sudo ./init.sh
-2. sudo ./run.sh
+1. sudo ./run.sh
 
 Redmine will be accessible via port 3000
--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if !(docker inspect -f '{{.Id}}' mysql-redmine &> /dev/null)
+then
+	./init.sh
+fi
+
 docker start mysql-redmine
 docker run --link mysql-redmine:mysql --rm -p 3000:3000 --name redmine ir2sora/redmine ruby script/rails server webrick -e production
